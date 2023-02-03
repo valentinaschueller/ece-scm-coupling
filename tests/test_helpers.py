0f8a089865a6d99731ca03d86bda9882cf73ea0c
Valentina Schueller <valentina.schueller@gmail.com>
2023-02-03 15:24:27 +0100

    Add test for NSTRTINI computation

---
+++ tests/test_helpers.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import pytest
+
+import helpers
+
+
+def test_compute_nstrtini():
+    forcing_start_date = pd.Timestamp("2014-07-01")
+    start_date = pd.Timestamp("2014-06-30")
+    pytest.raises(ValueError, helpers.compute_nstrtini, start_date, forcing_start_date)
+    start_date = pd.Timestamp("2014-07-01 03:00")
+    pytest.raises(ValueError, helpers.compute_nstrtini, start_date, forcing_start_date)
+    start_date = pd.Timestamp("2014-07-02")
+    assert helpers.compute_nstrtini(start_date, forcing_start_date) == 5
+    assert helpers.compute_nstrtini(start_date, forcing_start_date, 3) == 9
