6a3ab2d238eeed0a54a7f25509d4d035b2adc5ac
Valentina Schueller <valentina.schueller@gmail.com>
2023-02-04 16:50:20 +0100

    Remove Iris dependency and refactor remapping

--- remapping.py
+++ remapping.py
@@ -1,30 +1,6 @@
 from pathlib import Path
 
-import iris
-
-
-def remap_oce_to_atm(
-    oce_file_path: str, oce_var_name: str, atm_file_path: Path, atm_var_name: str
-):
-    oce_cube = iris.load_cube(oce_file_path, oce_var_name)
-    try:
-        atm_cube = oce_cube[:, :, [4]]
-    except IndexError:
-        tmp_cube = oce_cube[:, :, [1]]
-        atm_cube = tmp_cube[:, [1], :]
-    atm_cube.var_name = atm_var_name
-    iris.save(atm_cube, atm_file_path)
-
-
-def remap_atm_to_oce(
-    atm_file_path: str, atm_var_name: str, oce_file_path: Path, oce_var_name: str
-):
-    atm_cube = iris.load_cube(atm_file_path, atm_var_name)
-    tmp_cube = atm_cube[:, :, 3 * [0]]
-    oce_cube = tmp_cube[:, 3 * [0], :]
-    oce_cube.var_name = oce_var_name
-    iris.save(oce_cube, oce_file_path)
-
+import xarray as xr
 
 atm_to_oce = {
     "A_TauX_oce": "O_OTaux1",
@@ -50,3 +26,65 @@ oce_to_atm = {
     "OIceTck": "A_Ice_thickness",
     "OSnwTck": "A_Snow_thickness",
 }
+
+
+class RemapCouplerOutput:
+    """
+    Create input files for an upcoming Schwarz iteration.
+
+    Fields get remapped, renamed, and their time coordinate is updated as required.
+    """
+
+    def __init__(
+        self,
+        read_directory: Path,
+        write_directory: Path,
+        coupling_scheme: int,
+        dt_cpl: int,
+        dt_atm: int,
+        dt_oce: int,
+    ) -> None:
+        self.read_directory = read_directory
+        self.write_directory = write_directory
+        self.coupling_scheme = coupling_scheme
+        self.dt_cpl = dt_cpl
+        self.dt_atm = dt_atm
+        self.dt_oce = dt_oce
+
+    def remap(self) -> None:
+        for path in self.read_directory.glob("*.nc"):
+            if "_ATMIFS_" in path.stem:
+                self._remap_atm_to_oce(path)
+            if "_oceanx_" in path.stem:
+                self._remap_oce_to_atm(path)
+
+    def _remap_oce_to_atm(self, oce_file_path: Path) -> None:
+        oce_var_name = oce_file_path.stem.split("_oceanx_")[0]
+        atm_var_name = oce_to_atm.get(oce_var_name, None)
+        if atm_var_name is None:
+            return
+        oce_da = xr.open_dataarray(oce_file_path)
+        try:
+            atm_da = oce_da[:, :, [4]]
+        except IndexError:
+            atm_da = oce_da[:, [0], [0]]
+        atm_da = atm_da.rename(atm_var_name)
+        if self.coupling_scheme != 2:
+            atm_da = atm_da[1:]
+        atm_da = atm_da.assign_coords({"time": atm_da.time.data - (self.dt_cpl - self.dt_oce)})
+        atm_file_path = self.write_directory / f"{atm_var_name}.nc"
+        atm_da.to_netcdf(atm_file_path)
+
+    def _remap_atm_to_oce(self, atm_file_path: Path) -> None:
+        atm_var_name = atm_file_path.stem.split("_ATMIFS_")[0]
+        oce_var_name = atm_to_oce.get(atm_var_name, None)
+        if oce_var_name is None:
+            return
+        atm_da = xr.open_dataarray(atm_file_path)
+        oce_da = atm_da[:, 3 * [0], 3 * [0]]
+        oce_da = oce_da.rename(oce_var_name)
+        if self.coupling_scheme != 1:
+            oce_da = oce_da[1:]
+        oce_da = oce_da.assign_coords({"time": oce_da.time.data - (self.dt_cpl - self.dt_atm)})
+        oce_file_path = self.write_directory / f"{oce_var_name}.nc"
+        oce_da.to_netcdf(oce_file_path)
