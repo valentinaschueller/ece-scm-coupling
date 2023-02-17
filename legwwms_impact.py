6bf89d421167b5b2238b57e13ee4ccdae77a65ba
Valentina Schueller <valentina.schueller@gmail.com>
2023-02-17 11:17:48 +0100

    Fix LEGWWMS impact
    refactor parts of experiment setup into separate module
    
    Further refactoring of setup_experiment

--- legwwms_impact.py
+++ legwwms_impact.py
@@ -1,37 +1,22 @@
 import numpy as np
+import pandas as pd
 import proplot as pplt
 import xarray as xr
 
 import user_context as context
-import utils.helpers as hlp
 import utils.plotting as uplt
+from setup_experiment import set_experiment_date_properties, set_experiment_input_files
+from utils.files import NEMOPreprocessor, OIFSPreprocessor
+from utils.helpers import AOSCM
 from utils.templates import render_config_xml
-
-
-def generate_experiments(
-    exp_prefix: str, dt_cpl: int, dt_ifs: int, dt_nemo: int, cpl_scheme: int
-):
-    exp_setups = []
-    legwwms_vals = ["T", "F"]
-    for legwwms_val in legwwms_vals:
-        dct = {
-            "exp_id": f"{exp_prefix}{legwwms_val}",
-            "dt_cpl": dt_cpl,
-            "dt_nemo": dt_nemo,
-            "dt_ifs": dt_ifs,
-            "cpl_scheme": cpl_scheme,
-            "ifs_legwwms": legwwms_val,
-        }
-        exp_setups.append(dct)
-    return exp_setups
 
 
 def load_datasets(exp_ids: list):
     run_directories = [context.output_dir / exp_id for exp_id in exp_ids]
-    oifs_preprocessor = uplt.OIFSPreprocessor(
-        np.datetime64("2014-07-01"), np.timedelta64(-7, "h")
+    oifs_preprocessor = OIFSPreprocessor(
+        ifs_input_file_start_date, np.timedelta64(-7, "h")
     )
-    nemo_preprocessor = uplt.NEMOPreprocessor(np.timedelta64(-7, "h"))
+    nemo_preprocessor = NEMOPreprocessor(np.timedelta64(-7, "h"))
     oifs_progvars = [
         xr.open_mfdataset(
             run_directory / "progvar.nc", preprocess=oifs_preprocessor.preprocess
@@ -55,8 +40,7 @@ def load_datasets(exp_ids: list):
 
 
 def create_and_save_plots(exp_ids):
-    setup = "PAPA"
-    oifs_progvars, oifs_diagvars, nemo_t_grids = load_datasets(setup, exp_ids)
+    oifs_progvars, oifs_diagvars, nemo_t_grids = load_datasets(exp_ids)
 
     colors = ["k", "C8"]
     labels = ["GWD = TRUE", "GWD = FALSE"]
@@ -71,10 +55,15 @@ def create_and_save_plots(exp_ids):
     uplt.create_oce_ssts_plot(axs[1], nemo_t_grids, colors, alpha, labels, linestyles)
     uplt.create_atm_ssws_plot(axs[2], oifs_diagvars, colors, alpha, labels, linestyles)
     fig.savefig(
-        f"plots/legwwms_impact/{setup}_{exp_ids[0][:-1]}.pdf",
+        f"plots/legwwms_impact/{exp_ids[0][:-1]}.pdf",
         bbox_inches="tight",
     )
 
+
+start_date = pd.Timestamp("2014-07-01")
+simulation_duration = pd.Timedelta("4D")
+ifs_input_file_start_date = pd.Timestamp("2014-07-01")
+ifs_input_file_freq = pd.Timedelta("6H")
 
 dt_cpl = 3600
 dt_ifs = 900
@@ -83,15 +72,46 @@ dt_nemo = 1800
 cpl_scheme = 0
 exp_prefix = "GWD"
 
-experiments = generate_experiments(exp_prefix, dt_cpl, dt_ifs, dt_nemo, cpl_scheme)
-print(experiments)
+experiment = {
+    "dt_cpl": dt_cpl,
+    "dt_nemo": dt_nemo,
+    "dt_ifs": dt_ifs,
+    "cpl_scheme": cpl_scheme,
+}
+set_experiment_date_properties(
+    experiment,
+    start_date,
+    simulation_duration,
+    ifs_input_file_start_date,
+    ifs_input_file_freq,
+)
+set_experiment_input_files(experiment, start_date, "era")
 
+model = AOSCM(
+    context.runscript_dir,
+    context.ecconf_executable,
+    context.output_dir,
+    context.platform,
+)
 
-for experiment in experiments:
+legwwms_values = ["T", "F"]
+exp_ids = []
+
+for legwwms in legwwms_values:
+    experiment["ifs_legwwms"] = legwwms
+
+    exp_id = f"{exp_prefix}{legwwms}"
+    exp_ids.append(exp_id)
+    experiment["exp_id"] = exp_id
+    model.run_directory = context.output_dir / exp_id
+
     render_config_xml(context.runscript_dir, context.config_run_template, experiment)
+
     print(f"Config: {experiment['exp_id']}")
-    hlp.run_model()
-exp_ids = [experiment["exp_id"] for experiment in experiments]
+    model.run_coupled_model(print_time=False, schwarz_correction=False)
+
+    model.reduce_output()
+
 print("Creating plots")
 create_and_save_plots(exp_ids)
 print("Plots created!")
