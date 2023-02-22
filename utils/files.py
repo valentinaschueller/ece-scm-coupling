93938ddbd861b5ea3f817ad43eb940c5aa774f11
Valentina Schueller <valentina.schueller@gmail.com>
2023-02-22 14:26:37 +0100

    Various improvements in utils/files
    added docstrings to existing classes
    switched to less cluttered key=value syntax
    in calls to Dataset.assign_coords()
    (from {"key": value})
    
    fixed NEMOPreprocessor: should also take origin into account.
    I noticed that NEMO always starts its output at 00:00 even if the
    simulation starts at a different time.
    
    Fix OIFSEnsembleProcessor:
    Do not change time coordinate! Keep it relative
    to prevent unnecessary disk storage being wasted.
    Also include time_shift in start_date coordinate
    Reordering coordinates is now superfluous -> saves a lot of time!
    
    Add preprocessor for NEMO ensemble data
    - time_counter gets renamed to time
    - make time a relative coordinate as for OIFS ensemble data to save storage
    - to make it relative, take into account that NEMO always starts
    its output relative to 00:00 UTC (e.g., l. 178)

--- utils/files.py
+++ utils/files.py
@@ -1,13 +1,24 @@
 import os
 from pathlib import Path
 
+import numpy as np
 import pandas as pd
 import xarray as xr
 
 
-# Using https://stackoverflow.com/revisions/13197763/9
 class ChangeDirectory:
-    """Context manager for changing the current working directory"""
+    """Context manager for changing the current working directory.
+
+    Use as follows:
+    ```python
+    with ChangeDirectory(target_directory):
+        # this code will be executed inside `target_directory`
+        pass
+    # this code will be executed in the original working directory
+    ```
+
+    Implementation copied from [Brian M. Hunt](https://stackoverflow.com/revisions/13197763/9).
+    """
 
     def __init__(self, new_path):
         self.new_path = Path(new_path).expanduser()
@@ -21,32 +32,78 @@ class ChangeDirectory:
 
 
 class OIFSPreprocessor:
-    """Preprocessor for Output Data from the OpenIFS SCM."""
+    """Preprocessor for Output Data from the OpenIFS SCM.
+
+    - converts the `time` coordinate to valid datetime objects, computed relative to the simulation start date, `origin`
+    - optional: applies a time shift for local time zones using `time_shift`
+    """
 
     def __init__(
         self, origin: pd.Timestamp, time_shift: pd.Timedelta = pd.Timedelta(0)
     ):
+        """Constructor.
+
+        :param origin: start date (+ time) of the simulation
+        :type origin: pd.Timestamp
+        :param time_shift: time shift to apply for local time, defaults to pd.Timedelta(0)
+        :type time_shift: pd.Timedelta, optional
+        """
         self.origin = origin
         self.time_shift = time_shift
 
     def preprocess(self, ds: xr.Dataset) -> xr.Dataset:
-        fixed_ds = ds.assign_coords(
-            {"time": self.origin + ds.time.data + self.time_shift}
-        )
-        return fixed_ds
+        """Preprocess function for use with `xr.open_mfdataset()`.
+
+        :param ds: dataset as loaded from disk
+        :type ds: xr.Dataset
+        :return: preprocessed dataset
+        :rtype: xr.Dataset
+        """
+        ds = ds.assign_coords(time=self.origin + ds.time.data + self.time_shift)
+        return ds
 
 
 class NEMOPreprocessor:
-    """Preprocessor for Output Data from the NEMO SCM."""
+    """Preprocessor for Output Data from the NEMO SCM.
 
-    def __init__(self, time_shift: pd.Timedelta = pd.Timedelta(0)):
+    - renames the time coordinate to `time`
+    - sets the correct start date (NEMO always starts its output at 00:00 UTC)
+    - applies a time shift for local time zones
+    """
+
+    def __init__(
+        self, origin: pd.Timestamp, time_shift: pd.Timedelta = pd.Timedelta(0)
+    ):
+        """Constructor.
+
+        :param origin: start date (+ time) of the simulation
+        :type origin: pd.Timestamp
+        :param time_shift: time shift to apply for local time, defaults to pd.Timedelta(0)
+        :type time_shift: pd.Timedelta, optional
+        """
+        self.origin = origin
+        self.fix_start_date = False
+        if self.origin.hour != 0:
+            self.fix_start_date = True
         self.time_shift = time_shift
 
     def preprocess(self, ds: xr.Dataset) -> xr.Dataset:
-        fixed_ds = ds.assign_coords(
-            {"time_counter": ds.time_counter.data + self.time_shift}
-        )
-        return fixed_ds
+        """Preprocess function for use with `xr.open_mfdataset()`.
+
+        :param ds: dataset as loaded from disk
+        :type ds: xr.Dataset
+        :return: preprocessed dataset
+        :rtype: xr.Dataset
+        """
+        ds = ds.rename(time_counter="time")
+        if self.fix_start_date:
+            time_as_timedelta = ds.time.data - np.datetime64(self.origin.date())
+            ds = ds.assign_coords(
+                time=time_as_timedelta + self.origin + self.time_shift
+            )
+        else:
+            ds = ds.assign_coords(time=ds.time.data + self.time_shift)
+        return ds
 
 
 class OIFSEnsemblePreprocessor:
@@ -63,25 +120,11 @@ class OIFSEnsemblePreprocessor:
             source_file.parent.parent.parent.name.replace("_", ", ")
         )
         initial_condition = source_file.parent.parent.name
-        ds = ds.assign_coords(time=start_date + ds.time.data + self.time_shift)
         ds = ds.expand_dims(
             coupling_scheme=[coupling_scheme],
-            start_date=[start_date],
+            start_date=[start_date + self.time_shift],
             initial_condition=[initial_condition],
         )
-        # reorder coordinates to prevent monotonicity issue: https://github.com/pydata/xarray/issues/6355
-        ds = ds[
-            [
-                "time",
-                "start_date",
-                "coupling_scheme",
-                "initial_condition",
-                "nlev",
-                "nlevp1",
-                "nlevs",
-                *list(ds.data_vars),
-            ]
-        ]
         return ds
 
     def preprocess_schwarz_iterations(self, ds: xr.Dataset) -> xr.Dataset:
@@ -92,23 +135,50 @@ class OIFSEnsemblePreprocessor:
             source_file.parent.parent.parent.name.replace("_", ", ")
         )
         initial_condition = source_file.parent.parent.name
-        ds = ds.assign_coords(time=start_date + ds.time.data + self.time_shift)
         ds = ds.expand_dims(
-            start_date=[start_date],
+            start_date=[start_date + self.time_shift],
             initial_condition=[initial_condition],
             schwarz_iteration=[int(iteration)],
         )
-        # reorder coordinates to prevent monotonicity issue: https://github.com/pydata/xarray/issues/6355
-        ds = ds[
-            [
-                "time",
-                "start_date",
-                "schwarz_iteration",
-                "initial_condition",
-                "nlev",
-                "nlevp1",
-                "nlevs",
-                *list(ds.data_vars),
-            ]
-        ]
+        return ds
+
+
+class NEMOEnsemblePreprocessor:
+    def __init__(self, time_shift: pd.Timedelta = pd.Timedelta(0)):
+        self.time_shift = time_shift
+
+    def preprocess_ensemble(self, ds: xr.Dataset) -> xr.Dataset:
+        source_file = Path(ds.encoding["source"])
+        coupling_scheme = source_file.parent.name
+        if "schwarz" in coupling_scheme:
+            coupling_scheme = "converged SWR"
+
+        start_date = pd.Timestamp(
+            source_file.parent.parent.parent.name.replace("_", ", ")
+        )
+        initial_condition = source_file.parent.parent.name
+        ds = ds.rename(time_counter="time")
+        ds = ds.assign_coords(time=ds.time.data - np.datetime64(start_date.date()))
+        ds = ds.expand_dims(
+            coupling_scheme=[coupling_scheme],
+            start_date=[start_date + self.time_shift],
+            initial_condition=[initial_condition],
+        )
+        return ds
+
+    def preprocess_schwarz_iterations(self, ds: xr.Dataset) -> xr.Dataset:
+        source_file = Path(ds.encoding["source"])
+        _, iteration = source_file.parent.name.split("_")
+
+        start_date = pd.Timestamp(
+            source_file.parent.parent.parent.name.replace("_", ", ")
+        )
+        initial_condition = source_file.parent.parent.name
+        ds = ds.rename(time_counter="time")
+        ds = ds.assign_coords(time=ds.time.data - np.datetime64(start_date.date()))
+        ds = ds.expand_dims(
+            start_date=[start_date + self.time_shift],
+            initial_condition=[initial_condition],
+            schwarz_iteration=[int(iteration)],
+        )
         return ds
