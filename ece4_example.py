ce8d6dba3697069ffcc8be32b5f65dbd3fa72baf
Valentina Schüller <valentina.schuller@math.lu.se>
2024-03-08 11:06:38 +0100

    Update ECE4 example for Schwarz run

--- ece4_example.py
+++ ece4_example.py
@@ -3,6 +3,7 @@ import pandas as pd
 import user_context as context
 from setup_experiment import set_experiment_date_properties, set_experiment_input_files
 from utils.helpers import AOSCM, reduce_output
+from schwarz_coupling import SchwarzCoupling
 from utils.templates import render_config_xml
 
 cpl_scheme = 0
@@ -11,9 +12,9 @@ dt_ifs = 900
 dt_nemo = 900
 max_iters = 20
 
-start_date = pd.Timestamp("2014-07-01")
+start_date = pd.Timestamp("2010-06-15")
 simulation_duration = pd.Timedelta(12, "hours")
-ifs_input_start_date = pd.Timestamp("2014-07-01")
+ifs_input_start_date = pd.Timestamp("2010-06-15")
 ifs_input_freq = pd.Timedelta(6, "hours")
 
 experiment = {
@@ -21,11 +22,24 @@ experiment = {
     "dt_nemo": dt_nemo,
     "dt_ifs": dt_ifs,
     "ifs_leocwa": "F",
+    "model_dir": context.model_dir,
+    "run_dir": context.output_dir,
 }
 set_experiment_date_properties(
     experiment, start_date, simulation_duration, ifs_input_start_date, ifs_input_freq
 )
-set_experiment_input_files(experiment, start_date, "era")
+nem_input_file = context.nemo_input_files_dir / "init_PAPASTATION_2010-06-15.nc"
+oifs_input_file = context.ifs_input_files_dir / "papa_20100615.nc"
+oasis_rstas = context.rstas_dir / ifn.get_rstas_name(start_date, ifs_input_file_source)
+oasis_rstos = context.rstos_dir / ifn.get_rstos_name(start_date)
+
+assert nem_input_file.exists()
+assert oifs_input_file.exists()
+
+experiment["nem_input_file"] = nem_input_file
+experiment["ifs_input_file"] = oifs_input_file
+experiment["oasis_rstas"] = oasis_rstas
+experiment["oasis_rstos"] = oasis_rstos
 
 aoscm = AOSCM(
     context.runscript_dir,
@@ -34,15 +48,28 @@ aoscm = AOSCM(
 )
 
 
+ifs_input_start_date = pd.Timestamp("2010-06-15")
+ifs_input_freq = pd.Timedelta(6, "hours")
+
+max_iters = 10
+
 if __name__ == "__main__":
 
-    experiment["exp_id"] = f"TEST"
+    for cpl_scheme in [0, 1, 2]:
+        exp_id = f"TES{cpl_scheme}"
+        experiment["exp_id"] = exp_id
+        experiment["cpl_scheme"] = cpl_scheme
+        print(f"Config: {experiment['exp_id']}")
+        render_config_xml(
+            context.runscript_dir, context.config_run_template, experiment
+        )
+        aoscm.run_coupled_model()
+        reduce_output(run_directory=context.output_dir / exp_id)
+
+    cpl_scheme = 0
     experiment["cpl_scheme"] = cpl_scheme
-    print(f"Config: {experiment['exp_id']}")
-    render_config_xml(
-        context.runscript_dir, context.config_run_template, experiment
-    )
-    aoscm.run_coupled_model()
-    # reduce_output(
-    #     context.output_dir / experiment["exp_id"], keep_debug_output=False
-    # )
+    exp_id = "TESS"
+    experiment["exp_id"] = exp_id
+
+    schwarz_exp = SchwarzCoupling(experiment, reduce_output_after_iteration=True)
+    schwarz_exp.run(max_iters)
