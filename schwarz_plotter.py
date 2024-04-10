2aadb1ce665784b0ff990ec1d27106ab18affc27
Valentina Schüller <valentina.schuller@math.lu.se>
2024-04-10 11:12:59 +0200

    Refactor loading iterates

--- schwarz_plotter.py
+++ schwarz_plotter.py
@@ -9,6 +9,64 @@ from utils.files import OIFSPreprocessor, NEMOPreprocessor
 
 plt.style.use("stylesheet.mpl")
 
+
+def load_iterates(
+    file_name: str, preprocess: callable, max_iters: int, step: int
+) -> xr.Dataset:
+    swr_dim = xr.DataArray(np.arange(max_iters) + 1, dims="swr_iterate")
+    iterates = [
+        xr.open_mfdataset(f"PAPA/{exp_id}_{iter}/{file_name}", preprocess=preprocess)
+        for iter in range(1, max_iters + 1, step)
+    ]
+    iterates = xr.concat(iterates, swr_dim)
+    return iterates
+
+
+def plot_all_iterates(da: xr.DataArray, **kwargs):
+    fig, ax = plt.subplots()
+    da[0].plot(ax=ax)
+
+    ax.set(**kwargs)
+    ax.grid()
+    for iter in range(1, max_iters):
+        ax.plot(da.time, da[iter], alpha=alpha, color="k")
+
+    return fig
+
+
+def animate(da: xr.DataArray, **kwargs):
+    fig, ax = plt.subplots()
+    da[0].plot(ax=ax)
+
+    ax.set(**kwargs)
+    ax.grid()
+
+    def update(frame):
+        # update the line plot:
+        ax.plot(
+            da[frame + 1].time,
+            da[frame + 1],
+            color="k",
+            alpha=alpha,
+        )
+
+    ani = animation.FuncAnimation(fig=fig, func=update, frames=da.shape[0] - 1)
+    return ani
+
+
+def create_plots(da: xr.DataArray, file_stem: str, axis_settings: dict):
+    axis_settings["xlabel"] = "Time"
+    axis_settings["xmargin"] = 0.0
+
+    fig = plot_all_iterates(da, **axis_settings)
+    fig.savefig(plot_folder / f"{file_stem}.pdf", bbox_inches="tight")
+
+    ani = animate(da, **axis_settings)
+    ani.save(plot_folder / f"{file_stem}.mp4")
+
+
+# %%
+
 start_date = experiment_runner.start_date
 time_shift = np.timedelta64(1, "h")
 oifs_preprocessor = OIFSPreprocessor(start_date, time_shift)
@@ -25,79 +83,22 @@ if sequential_swr:
     step = 2
 else:
     step = 1
-oifs_diagvars = [
-    xr.open_mfdataset(
-        f"PAPA/{exp_id}_{iter}/diagvar.nc", preprocess=oifs_preprocessor.preprocess
-    )
-    for iter in range(1, max_iters + 1, step)
-]
-oifs_progvars = [
-    xr.open_mfdataset(
-        f"PAPA/{exp_id}_{iter}/progvar.nc", preprocess=oifs_preprocessor.preprocess
-    )
-    for iter in range(1, max_iters + 1, step)
-]
-nemo_t_grids = [
-    xr.open_mfdataset(
-        f"PAPA/{exp_id}_{iter}/{exp_id}_*_T.nc", preprocess=nemo_preprocessor.preprocess
-    )
-    for iter in range(1, max_iters + 1, step)
-]
-nemo_ice_grids = [
-    xr.open_mfdataset(
-        f"PAPA/{exp_id}_{iter}/{exp_id}*_icemod.nc",
-        preprocess=nemo_preprocessor.preprocess,
-    )
-    for iter in range(1, max_iters + 1, step)
-]
 
-
-def plot_all_iterates(da_list: list[xr.DataArray], **kwargs):
-    fig, ax = plt.subplots()
-    da_list[0].plot(ax=ax)
-
-    ax.set(**kwargs)
-    ax.grid()
-
-    for da in da_list[1:]:
-        ax.plot(da.time, da, alpha=alpha, color="k")
-
-    return fig
-
-
-def animate(da_list: list[xr.DataArray], **kwargs):
-    fig, ax = plt.subplots()
-    da_list[0].plot(ax=ax)
-
-    ax.set(**kwargs)
-    ax.grid()
-
-    def update(frame):
-        # update the line plot:
-        ax.plot(
-            da_list[frame + 1].time,
-            da_list[frame + 1],
-            color="k",
-            alpha=alpha,
-        )
-
-    ani = animation.FuncAnimation(fig=fig, func=update, frames=len(da_list) - 1)
-    return ani
-
-
-def create_plots(da_list: list[xr.DataArray], file_stem: str, axis_settings: dict):
-    axis_settings["xlabel"] = "Time"
-    axis_settings["xmargin"] = 0.0
-
-    fig = plot_all_iterates(da_list, **axis_settings)
-    fig.savefig(plot_folder / f"{file_stem}.pdf", bbox_inches="tight")
-
-    ani = animate(da_list, **axis_settings)
-    ani.save(plot_folder / f"{file_stem}.mp4")
+oifs_diagvars = load_iterates(
+    "diagvar.nc", oifs_preprocessor.preprocess, max_iters, step
+)
+oifs_progvars = load_iterates(
+    "progvar.nc", oifs_preprocessor.preprocess, max_iters, step
+)
+nemo_t_grids = load_iterates(
+    f"{exp_id}_*_T.nc", nemo_preprocessor.preprocess, max_iters, step
+)
+nemo_ice_grids = load_iterates(
+    f"{exp_id}_*_icemod.nc", nemo_preprocessor.preprocess, max_iters, step
+)
 
 
 # %% 10m Temperature
-temp_10m = [progvar.t[:, 59] - 273.15 for progvar in oifs_progvars]
 
 axis_settings = {
     "title": "Temperature at 10m (OIFS)",
@@ -105,32 +106,28 @@ axis_settings = {
     "ylim": [-80, -35],
 }
 
-create_plots(temp_10m, "10t_oifs", axis_settings)
+create_plots(oifs_progvars.t[:, :, 59] - 273.15, "10t_oifs", axis_settings)
 
 
 # %% Surface Sensible Heat Flux
-ssh_flux = [diagvar.sfc_sen_flx for diagvar in oifs_diagvars]
-
 axis_settings = {
     "title": "Surface Sensible Heat Flux (OIFS)",
     "ylabel": "Heat Flux $[W m^{-2}]$",
     "ylim": [-1400, 200],
 }
 
-create_plots(ssh_flux, "ssh_oifs", axis_settings)
+create_plots(oifs_diagvars.sfc_sen_flx, "ssh_oifs", axis_settings)
 
 # %% SST
 
-sst = [t_grid.sosstsst for t_grid in nemo_t_grids]
 axis_settings = {
     "title": "Sea Surface Temperature (NEMO)",
     "ylabel": "Temperature [°C]",
     "ylim": [-2.1, -1.3],
 }
-create_plots(sst, "sst_nemo", axis_settings)
+create_plots(nemo_t_grids.sosstsst, "sst_nemo", axis_settings)
 
 # %% SSW
-ssw = [oifs_diagvar.sfc_swrad for oifs_diagvar in oifs_diagvars]
 
 axis_settings = {
     "title": "Surface SW Radiation (OIFS)",
@@ -138,11 +135,10 @@ axis_settings = {
     "ylim": [0, 80],
 }
 
-create_plots(ssw, "ssw_oifs", axis_settings)
+create_plots(oifs_diagvars.sfc_swrad, "ssw_oifs", axis_settings)
 
 # %% Sea Ice Concentration
 
-iceconc = [ice_grid.iceconc for ice_grid in nemo_ice_grids]
 
 axis_settings = {
     "title": "Sea Ice Concentration (LIM3)",
@@ -150,11 +146,9 @@ axis_settings = {
     "ylim": [0.99, 1],
 }
 
-create_plots(iceconc, "iceconc_lim3", axis_settings)
+create_plots(nemo_ice_grids.iceconc, "iceconc_lim3", axis_settings)
 
 # %% Ice Surface Temperature
-
-icest = [ice_grid.icest for ice_grid in nemo_ice_grids]
 
 axis_settings = {
     "title": "Sea Ice Surface Temperature (LIM3)",
@@ -162,11 +156,9 @@ axis_settings = {
     "ylim": [-90, 5],
 }
 
-create_plots(icest, "icest_lim3", axis_settings)
+create_plots(nemo_ice_grids.icest, "icest_lim3", axis_settings)
 
 # %% Mean Ice Temperature
-
-micet = [ice_grid.micet for ice_grid in nemo_ice_grids]
 
 axis_settings = {
     "title": "Mean Sea Ice Temperature (LIM3)",
@@ -174,6 +166,29 @@ axis_settings = {
     "ylim": [-10, -6],
 }
 
-create_plots(micet, "micet_lim3", axis_settings)
+create_plots(nemo_ice_grids.micet, "micet_lim3", axis_settings)
 
+# %%
+
+axis_settings = {
+    "title": "Total Ice Heat Content (LIM3)",
+    "ylabel": "Heat Content [J]",
+    "ylim": [5.8e8, 6.25e8],
+}
+
+create_plots(nemo_ice_grids.icehc, "icehc_lim3", axis_settings)
+# %%
+
+for category in range(1, 6):
+    axis_settings = {
+        "title": f"Concentration of Thickness Category {category} (LIM3)",
+        "ylabel": "Sea Ice Concentration [-]",
+        "ylim": [0, 1],
+    }
+
+    create_plots(
+        nemo_ice_grids.iceconc_cat.sel(ncatice=category),
+        f"iceconc_cat{category}_lim3",
+        axis_settings,
+    )
 # %%
