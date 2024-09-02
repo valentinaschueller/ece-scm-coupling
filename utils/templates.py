3e21a9b9eabbe8b894a1b7ac14be45f725315a98
Valentina Schüller <valentina.schuller@math.lu.se>
2024-09-02 16:41:48 +0200

    Formatting

--- utils/templates.py
+++ utils/templates.py
@@ -1,7 +1,8 @@
-from context import Context
 from pathlib import Path
 
 import jinja2
+
+from context import Context
 
 
 def get_template(template_path: Path) -> jinja2.Template:
@@ -11,9 +12,7 @@ def get_template(template_path: Path) -> jinja2.Template:
     return environment.get_template(template_path.name)
 
 
-def render_config_xml(
-    context: Context, experiment: dict
-) -> None:
+def render_config_xml(context: Context, experiment: dict) -> None:
     jinja_template = get_template(context.config_run_template)
     with open(context.runscript_dir / "config-run.xml", "w") as config_run_xml:
         config_run_xml.write(
