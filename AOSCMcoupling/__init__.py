d3dc0590edbd19308d49a603eef996dfd4962bdd
Valentina Schüller <valentina.schuller@math.lu.se>
2024-09-11 08:06:57 +0200

    Formatting + import ConvergenceChecker in __init__.py

--- AOSCMcoupling/__init__.py
+++ AOSCMcoupling/__init__.py
@@ -1,6 +1,7 @@
 from AOSCMcoupling.context import Context
-from AOSCMcoupling.schwarz_coupling import SchwarzCoupling
+from AOSCMcoupling.convergence_checker import ConvergenceChecker
 from AOSCMcoupling.experiment import Experiment
-from AOSCMcoupling.files import OIFSPreprocessor, NEMOPreprocessor, OASISPreprocessor
+from AOSCMcoupling.files import NEMOPreprocessor, OASISPreprocessor, OIFSPreprocessor
 from AOSCMcoupling.helpers import AOSCM, compute_nstrtini, reduce_output
+from AOSCMcoupling.schwarz_coupling import SchwarzCoupling
 from AOSCMcoupling.templates import render_config_xml
