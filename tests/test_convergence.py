b0d4619c6c4bc029403886a825436ca19d16308f
Valentina Schüller <valentina.schuller@gmail.com>
2024-10-28 18:16:10 +0100

    Simple tests for convergence checker module

---
+++ tests/test_convergence.py
@@ -0,0 +1,19 @@
+import numpy as np
+import xarray as xr
+from AOSCMcoupling.convergence_checker import vector_norm, relative_criterion
+
+
+def test_vector_norm():
+    arr = np.random.rand(5)
+    da = xr.DataArray(arr)
+    assert np.isclose(np.linalg.norm(arr), vector_norm(da, "dim_0"))
+    assert np.isclose(np.linalg.norm(arr, np.inf), vector_norm(da, "dim_0", np.inf))
+
+
+def test_relative_criterion():
+    arr_1 = np.random.rand(5)
+    arr_2 = arr_1 * (1 + 1e-4)
+    da_1 = xr.DataArray(arr_1, dims="time")
+    da_2 = xr.DataArray(arr_2, dims="time")
+    assert relative_criterion(da_1, da_2, da_1, 1e-3)
+    assert not relative_criterion(da_1, da_2, da_1, 1e-5)
