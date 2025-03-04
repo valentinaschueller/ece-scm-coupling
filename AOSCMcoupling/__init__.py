1b0366b8ca888dd33ccab80c14890bb937a88982
Valentina Schüller <valentina.schuller@math.lu.se>
2025-03-04 14:38:36 +0100

    Expose get_ifs_forcing_info via __init__.py

--- AOSCMcoupling/__init__.py
+++ AOSCMcoupling/__init__.py
@@ -2,6 +2,6 @@ from AOSCMcoupling.context import Context
 from AOSCMcoupling.convergence_checker import ConvergenceChecker
 from AOSCMcoupling.experiment import Experiment
 from AOSCMcoupling.files import NEMOPreprocessor, OASISPreprocessor, OIFSPreprocessor
-from AOSCMcoupling.helpers import AOSCM, compute_nstrtini, reduce_output
+from AOSCMcoupling.helpers import AOSCM, compute_nstrtini, reduce_output, get_ifs_forcing_info
 from AOSCMcoupling.schwarz_coupling import SchwarzCoupling
 from AOSCMcoupling.templates import render_config_xml
