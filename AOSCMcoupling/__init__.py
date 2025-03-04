e7df445c6010635b8e061c20aac0d26256be20ac
Valentina Schüller <valentina.schuller@math.lu.se>
2025-03-04 16:31:04 +0100

    Formatting

--- AOSCMcoupling/__init__.py
+++ AOSCMcoupling/__init__.py
@@ -2,6 +2,11 @@ from AOSCMcoupling.context import Context
 from AOSCMcoupling.convergence_checker import ConvergenceChecker
 from AOSCMcoupling.experiment import Experiment
 from AOSCMcoupling.files import NEMOPreprocessor, OASISPreprocessor, OIFSPreprocessor
-from AOSCMcoupling.helpers import AOSCM, compute_nstrtini, reduce_output, get_ifs_forcing_info
+from AOSCMcoupling.helpers import (
+    AOSCM,
+    compute_nstrtini,
+    get_ifs_forcing_info,
+    reduce_output,
+)
 from AOSCMcoupling.schwarz_coupling import SchwarzCoupling
 from AOSCMcoupling.templates import render_config_xml
